8c91d764a8e7861fd0f4c065e0aff3f91190bbf6
Milu7779 <milumathew7779@gmail.com>
2024-08-22 19:28:41 +0530

    to add wizard button on order line page and fetch and edit selected product then add on order line

--- sale_custom/wizard/wizard_add_product.py
+++ sale_custom/wizard/wizard_add_product.py
@@ -5,6 +5,18 @@ class AddProductLines(models.TransientModel):
 
     product_id = fields.Many2many('product.product', string='Product', required=True)
     sale_wizard_id = fields.Many2one('sale.order', string='sale wizard')
+    line_ids = fields.One2many('sale.order.line.wizard.line', 'wizard_id', string='Order Lines')
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        line_values = [(0, 0, {
+            'product_id': product.id,
+            'product_uom_qty': 1,
+            'price_unit': product.lst_price,
+        }) for product in self.product_id]
+        self.line_ids = line_values
+        print('ochage......', line_values)
+
 
     def add_products(self):
         sale_order = self.sale_wizard_id
@@ -13,5 +25,15 @@ class AddProductLines(models.TransientModel):
                 'order_id': sale_order.id,
                 'product_id': product.id,
                 'product_uom_qty': 1,
+                'price_unit': product.lst_price,
             })
         return {'type': 'ir.actions.act_window_close'}
+
+class SaleOrderLineWizardLine(models.TransientModel):
+    _name = 'sale.order.line.wizard.line'
+    _description = 'Sale Order Line Wizard Line'
+
+    wizard_id = fields.Many2one('product.select.wizard', string='Wizard Reference')
+    product_id = fields.Many2one('product.product', string='Product', required=True)
+    product_uom_qty = fields.Float(string='Quantity', default=1.0)
+    price_unit = fields.Float(string='Unit Price')
